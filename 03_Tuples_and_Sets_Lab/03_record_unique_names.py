0bb75af3c34a3713255f365ed8c6c5c00934eb34
Anton Petrov <a.petrov.83@gmail.com>
2022-12-17 18:02:13 +0200

    Added solution for exam "03. Record Unique Names" from "Tuples and Sets - Lab"

--- 03_Tuples_and_Sets_Lab/03_record_unique_names.py
+++ 03_Tuples_and_Sets_Lab/03_record_unique_names.py
@@ -4,7 +4,7 @@ n = int(input())
 names_set = set()
 
 # read input in range of 'n' and add input to names_set
-[names_set.add(input()) for name in range(n)]
+[names_set.add(input()) for _ in range(n)]
 
 # print all names stored in names_set
 [print(name) for name in names_set]
