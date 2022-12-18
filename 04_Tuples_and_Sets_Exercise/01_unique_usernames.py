c81feeb1998dc0ac0c12049ae33f0c340102a977
Anton Petrov <a.petrov.83@gmail.com>
2022-12-18 09:58:28 +0200

    Added solution for exam "01. Unique Usernames" from "Tuples and Sets - Exercise"

--- 04_Tuples_and_Sets_Exercise/01_unique_usernames.py
+++ 04_Tuples_and_Sets_Exercise/01_unique_usernames.py
@@ -1,6 +1,7 @@
 n = int(input())
 
-# reads input from console n-times and adds to set. This will ensure that we only record the unique names.
+# reads input from console n-times and adds them to set.
+# using set will ensure that we only record the unique names.
 unique_names = set(input() for x in range(n))
 
 # prints all stored names in unique_names set
