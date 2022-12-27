50b0366367d74eb4429bba49de91968dcb2314bf
Anton Petrov <a.petrov.83@gmail.com>
2022-12-27 20:15:36 +0200

    Added solution for exam "05. Santa's Present Factory" from "Stacks, Queues, Tuples and Sets - Exercise"

---
+++ 05_Stacks_Queues_Tuples_and_Sets_Exercise/05_santas_present_factory.py
@@ -0,0 +1,83 @@
+from collections import deque
+
+
+def presents_pair():
+    """
+    This function checks if manage to craft either one of the pairs:
+    - a doll and a train
+    - a teddy bear and a bicycle
+    :returns: boolean
+    """
+    if ('Doll' in crafted_toys and 'Wooden train' in crafted_toys) or \
+            ('Teddy bear' in crafted_toys and 'Bicycle' in crafted_toys):
+        return True
+    return False
+
+
+# read materials from console input in stack
+material_boxes = [int(x) for x in input().split()]
+
+# read magic from console input in queue
+magic_values = deque(int(x) for x in input().split())
+
+presents_table = {
+    150: 'Doll',
+    250: 'Wooden train',
+    300: 'Teddy bear',
+    400: 'Bicycle',
+}
+
+# create dict where to store crafted toys and count of each toy
+crafted_toys = {}
+
+# Stop crafting presents when run out of boxes of materials or magic level values.
+while material_boxes and magic_values:
+    material = material_boxes.pop()
+    magic = magic_values.popleft()
+
+    # If the magic or material (or both) equals 0, remove it (or both) and continue crafting the presents.
+    if magic == 0 and material == 0:
+        continue
+    if magic == 0:
+        material_boxes.append(material)
+        continue
+    if material == 0:
+        magic_values.appendleft(magic)
+        continue
+
+    # calculates total magic level
+    magic_level = material * magic
+
+    # if magic level is in presents_table dict values craft toy and add to crafted toys dict
+    if magic_level in presents_table:
+        crafted_toy = presents_table[magic_level]
+        if crafted_toy not in crafted_toys:
+            crafted_toys[crafted_toy] = 1
+        else:
+            crafted_toys[crafted_toy] += 1
+        continue
+
+    # If the product of the operation is a negative number, you should sum the values together,
+    # remove them both from their positions, and add the result to the materials.
+    if magic_level < 0:
+        material_boxes.append(material + magic)
+    # If the product doesn't equal one of the magic levels in the table and is a positive number,
+    # remove only the magic value and increase the material value by 15.
+    else:
+        material_boxes.append(material + 15)
+
+# On the first line - print whether you've succeeded in crafting the presents:
+if presents_pair():
+    print('The presents are crafted! Merry Christmas!')
+else:
+    print('No presents this Christmas!')
+
+# On the next two lines print the materials and magic that are left, if there are any (otherwise skip the line)
+if material_boxes:
+    print(f'Materials left: {", ".join(map(str, reversed(material_boxes)))}')
+if magic_values:
+    print(f'Magic left: {", ".join(map(str, magic_values))}')
+
+# On the next lines print the presents you have crafted, ordered alphabetically
+for toy, count in sorted(crafted_toys.items()):
+    print(f'{toy}: {count}')
