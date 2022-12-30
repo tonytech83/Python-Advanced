0a1659edb2c5a5a0409285239e48869fb42ded31
Anton Petrov <a.petrov.83@gmail.com>
2022-12-30 17:21:30 +0200

    Updated solution for exam "01. Diagonals" from "Multidimensional Lists - Exercise 1"

--- 07_Multidimensional_Lists_Exercise_1/01_diagonals.py
+++ 07_Multidimensional_Lists_Exercise_1/01_diagonals.py
@@ -1,6 +1,6 @@
 def read_matrix():
     """
-    Read size and rows of matrix
+    Read size of square matrix and returns read from console rows by received size.
     """
     size = int(input())
     return [[int(x) for x in input().split(', ')] for _ in range(size)]
@@ -8,24 +8,20 @@ def read_matrix():
 
 def find_diagonals(matrix):
     """
-    Find the matrix's diagonals and their sum.
+    Find the matrix's diagonals and returns them in two lists.
     """
     rows = len(matrix)
 
     primary = []
     secondary = []
-    primary_sum = 0
-    secondary_sum = 0
     for idx in range(rows):
         primary.append(matrix[idx][idx])
-        primary_sum += matrix[idx][idx]
         secondary.append(matrix[idx][rows - idx - 1])
-        secondary_sum += matrix[idx][rows - idx - 1]
 
-    return primary, primary_sum, secondary, secondary_sum
+    return primary, secondary
 
 
 matrix = read_matrix()
-primary_diagonal, primary_diagonal_sum, secondary_diagonal, secondary_diagonal_sum = find_diagonals(matrix)
-print(f'Primary diagonal: {", ".join(map(str, primary_diagonal))}. Sum: {primary_diagonal_sum}')
-print(f'Secondary diagonal: {", ".join(map(str, secondary_diagonal))}. Sum: {secondary_diagonal_sum}')
+primary_diagonal, secondary_diagonal = find_diagonals(matrix)
+print(f'Primary diagonal: {", ".join(map(str, primary_diagonal))}. Sum: {sum(primary_diagonal)}')
+print(f'Secondary diagonal: {", ".join(map(str, secondary_diagonal))}. Sum: {sum(secondary_diagonal)}')
