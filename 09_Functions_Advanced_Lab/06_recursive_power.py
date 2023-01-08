9608f1ec46da7c43f89c81d743f4d47d607b8ed9
Anton Petrov <a.petrov.83@gmail.com>
2023-01-08 08:58:12 +0200

    Updated solution for exam "06. Recursive Power" from "Functions Advanced - Lab"

---
+++ 09_Functions_Advanced_Lab/06_recursive_power.py
@@ -0,0 +1,10 @@
+def recursive_power(number, power):
+    if power == 1:
+        return number
+
+    return number * recursive_power(number, power - 1)
+
+
+# Test codes
+print(recursive_power(2, 10))
+print(recursive_power(10, 100))
