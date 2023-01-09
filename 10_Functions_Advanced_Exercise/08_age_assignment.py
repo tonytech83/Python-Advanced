dd4705d7a041accb3ace700017e9cf28a454d34d
Anton Petrov <a.petrov.83@gmail.com>
2023-01-09 14:39:19 +0200

    Added solution for exam "08. Age Assignment" from "Functions Advanced - Exercise"

--- 10_Functions_Advanced_Exercise/08_age_assignment.py
+++ 10_Functions_Advanced_Exercise/08_age_assignment.py
@@ -9,5 +9,6 @@ def age_assignment(*args, **kwargs):
     return '\n'.join(sorted_persons)
 
 
+# Test codes
 print(age_assignment("Peter", "George", G=26, P=19))
 print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
