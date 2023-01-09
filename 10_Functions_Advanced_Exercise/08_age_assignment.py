73d54a12a6ddae2bca137316b89a8f4277b468f8
Anton Petrov <a.petrov.83@gmail.com>
2023-01-09 14:39:02 +0200

    Added solution for exam "08. Age Assignment" from "Functions Advanced - Exercise"

---
+++ 10_Functions_Advanced_Exercise/08_age_assignment.py
@@ -0,0 +1,13 @@
+def age_assignment(*args, **kwargs):
+    persons = {}
+    for name in args:
+        first_letter = name[0]
+        if first_letter in kwargs:
+            age = kwargs[first_letter]
+            persons[name] = age
+    sorted_persons = [f'{n} is {a} years old.' for n, a in sorted(persons.items(), key=lambda kvp: kvp[0])]
+    return '\n'.join(sorted_persons)
+
+
+print(age_assignment("Peter", "George", G=26, P=19))
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
