5777d20e9c2bc04da0f1216bbd67fab61c3780e5
Anton Petrov <a.petrov.83@gmail.com>
2023-01-10 20:05:32 +0200

    Updated exam "03. Supermarket" form "Lists as Stacks and Queues - Lab"

--- 01_Lists_as_Stacks_and_Queues_Lab/03_supermarket.py
+++ 01_Lists_as_Stacks_and_Queues_Lab/03_supermarket.py
@@ -1,15 +1,17 @@
 from collections import deque
 
 customers = deque()
+COMMAND_END = 'END'
+COMMAND_PAID = 'Paid'
 
 while True:
     customer_name = input()
 
-    if customer_name == 'End':
+    if customer_name == COMMAND_END:
         print(f'{len(customers)} people remaining.')
         break
 
-    if customer_name == 'Paid':
+    if customer_name == COMMAND_PAID:
         while customers:
             print(customers.popleft())
         continue
