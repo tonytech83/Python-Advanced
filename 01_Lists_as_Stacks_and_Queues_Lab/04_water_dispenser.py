4f00e87daa1a9f193bcef6463324bba34e964956
Anton Petrov <a.petrov.83@gmail.com>
2023-01-10 20:34:30 +0200

    Updated exam "04. Water Dispenser" form "Lists as Stacks and Queues - Lab"

--- 01_Lists_as_Stacks_and_Queues_Lab/04_water_dispenser.py
+++ 01_Lists_as_Stacks_and_Queues_Lab/04_water_dispenser.py
@@ -1,27 +1,45 @@
 from collections import deque
 
+
+def add_people_in_queue():
+    """
+    Reads person name and added it in people queue while received 'Start'.
+    """
+    queue = deque()
+    while True:
+        current_name = input()
+
+        if current_name == COMMAND_START:
+            break
+        queue.append(current_name)
+
+    return queue
+
+
+# constants
+COMMAND_END = 'End'
+COMMAND_START = 'Start'
+COMMAND_REFILL = 'refill'
+
 # received quantity of water from console input
 dispenser = int(input())
 
 # create queue to store persons
-people = deque()
-
-# read person name and added it in people queue while received 'Start'
-while True:
-    name = input()
-    if name == 'Start':
-        break
-    people.append(name)
+people = add_people_in_queue()
 
 # brings water to person in the left of queue or refill the dispenser, while received 'End'.
 while True:
     command = input()
-    if command == 'End':
+    if command == COMMAND_END:
         print(f'{dispenser} liters left')
         break
 
-    if command.isdigit():
+    # if command is refill adds liters to dispenser
+    if command.split()[0] == COMMAND_REFILL:
+        refill_liters = int(command.split()[1])
+        dispenser += refill_liters
 
+    else:
         liters = int(command)
         person = people.popleft()
 
@@ -32,8 +50,3 @@ while True:
             dispenser -= liters
         else:
             print(f'{person} must wait')
-
-    # if command is refill adds liters to dispenser
-    else:
-        refill_liters = int(command.split()[1])
-        dispenser += refill_liters
