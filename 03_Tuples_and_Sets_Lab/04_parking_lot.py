5f40b60a27c5acc4dbc9362a5b43ce4d04d9f9b0
Anton Petrov <a.petrov.83@gmail.com>
2023-01-14 15:07:12 +0200

    Updated exam "04. Parking Lot" from "Tuples and Sets - Lab"

--- 03_Tuples_and_Sets_Lab/04_parking_lot.py
+++ 03_Tuples_and_Sets_Lab/04_parking_lot.py
@@ -1,21 +1,28 @@
+def output():
+    if parking_lot:
+        [print(car_plate) for car_plate in parking_lot]
+    else:
+        print('Parking Lot is Empty')
+
+
 n = int(input())
 
 # create set where to store the car numbers
 parking_lot = set()
+DIRECTION_IN = 'IN'
+DIRECTION_OUT = 'OUT'
 
 # iterates in range of 'n'
 for _ in range(n):
     # unpack input to direction and car_number
     direction, car_number = input().split(', ')
+
     # if direction is 'IN' adds car_number in parking_lot set
-    if direction == 'IN':
+    if direction == DIRECTION_IN:
         parking_lot.add(car_number)
+
     # if direction is 'OUT' removes car_number from parking_lot set
-    else:
+    elif direction == DIRECTION_OUT:
         parking_lot.remove(car_number)
 
-# if parking_lot is not empty, prints car_numbers inside
-if parking_lot:
-    [print(car_number) for car_number in parking_lot]
-else:
-    print('Parking Lot is Empty')
+output()
