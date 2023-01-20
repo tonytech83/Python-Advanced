04f17c2d5dcc5cf782eb6b1bf208eb8aace22ebc
Anton Petrov <a.petrov.83@gmail.com>
2023-01-20 19:08:41 +0200

    Updated exam "02. Diagonal Difference" from "Multidimensional Lists - Exercise 1"

--- 07_Multidimensional_Lists_Exercise_1/02_diagonal_difference.py
+++ 07_Multidimensional_Lists_Exercise_1/02_diagonal_difference.py
@@ -6,20 +6,17 @@ def read_matrix():
     return [[int(x) for x in input().split()] for _ in range(size)]
 
 
-def diagonals_difference(matrix):
+def diagonals_difference():
     """
     Find the absolute difference between the primary and the secondary diagonal sums.
     """
     rows = len(matrix)
 
-    primary_sum = 0
-    secondary_sum = 0
-    for idx in range(rows):
-        primary_sum += matrix[idx][idx]
-        secondary_sum += matrix[idx][rows - idx - 1]
+    primary_sum = sum([matrix[idx][idx] for idx in range(rows)])
+    secondary_sum = sum([matrix[idx][rows - idx - 1] for idx in range(rows)])
 
     return abs(primary_sum - secondary_sum)
 
 
 matrix = read_matrix()
-print(diagonals_difference(matrix))
+print(diagonals_difference())
