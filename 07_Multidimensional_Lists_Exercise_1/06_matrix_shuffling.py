25d6b41464a08dd66f5b5e54779dfd5cd6e81707
Anton Petrov <a.petrov.83@gmail.com>
2023-01-20 20:14:53 +0200

    Updated exam "06. Matrix Shuffling" from "Multidimensional Lists - Exercise 1"

--- 07_Multidimensional_Lists_Exercise_1/06_matrix_shuffling.py
+++ 07_Multidimensional_Lists_Exercise_1/06_matrix_shuffling.py
@@ -5,41 +5,44 @@ def read_matrix(rows):
     return [[x for x in input().split()] for _ in range(rows)]
 
 
-def valid_indexes(command):
+def valid_indexes(indexes):
     """
     Checks if coordinates are digits and are inside matrix.
     """
-    row1, col1, row2, col2 = [x for x in command[1:]]
-    if row1.isdigit() and col1.isdigit() and row2.isdigit() and col2.isdigit():
-        if int(row1) < rows and int(row2) < rows and int(col1) < columns and int(col2) < columns:
-            return True
+    if set(indexes[:2]).issubset(valid_rows) and set(indexes[2:]).issubset(valid_cols):
+        return True
+
     return False
 
 
-def valid_format(command):
+def valid_format(indexes):
     """
     Check if command is in valid format (contains 5 elements).
     """
-    if len(command) == 5:
+    if len(indexes) == 4:
         return True
+
     return False
 
 
 rows, columns = [int(x) for x in input().split()]
 matrix = read_matrix(rows)
+valid_rows = range(rows + 1)
+valid_cols = range(columns + 1)
 
 while True:
-    command = input().split()
-    if command[0] == 'END':
+    event, *data = [int(x) if x.isdigit() else x for x in input().split()]
+
+    if event == 'END':
         break
 
-    if command[0] != 'swap' or not valid_format(command):
+    if event != 'swap' or not valid_format(data):
         print('Invalid input!')
         continue
 
-    if valid_indexes(command):
-        matrix[int(command[1])][int(command[2])], matrix[int(command[3])][int(command[4])] = \
-            matrix[int(command[3])][int(command[4])], matrix[int(command[1])][int(command[2])]
+    if valid_indexes(data):
+        matrix[int(data[0])][int(data[1])], matrix[int(data[2])][int(data[3])] = \
+            matrix[int(data[2])][int(data[3])], matrix[int(data[0])][int(data[1])]
         for row in matrix:
             print(*row, sep=' ')
     else:
