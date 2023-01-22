5e3e9a0920ab17ec6eabfc78c211980dca98fe7d
Anton Petrov <a.petrov.83@gmail.com>
2023-01-22 14:17:44 +0200

    Added exam "01. File Opener" from "File Handling - Lab"

---
+++ 13_File_Handling_Lab/01_file_opener.py
@@ -0,0 +1,13 @@
+import os
+
+# take the absolute path to the directory where is stored .py file
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+
+# take the path to file which want ot open with os.path.join (works for all OS types)
+file_path = os.path.join(absolute_path, 'text.txt')
+
+try:
+    file_obj = open(file_path)
+    print('File found')
+except FileNotFoundError:
+    print('File not found')
