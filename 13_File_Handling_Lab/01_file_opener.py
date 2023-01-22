9813a893e2534b146dfbd6b165f83da47a54d9b3
Anton Petrov <a.petrov.83@gmail.com>
2023-01-22 14:29:13 +0200

    Updated exam "01. File Opener" from "File Handling - Lab"

--- 13_File_Handling_Lab/01_file_opener.py
+++ 13_File_Handling_Lab/01_file_opener.py
@@ -4,7 +4,7 @@ import os
 absolute_path = os.path.dirname(os.path.abspath(__file__))
 
 # take the path to file which want ot open with os.path.join (works for all OS types)
-file_path = os.path.join(absolute_path, 'text.txt')
+file_path = os.path.join(absolute_path, 'files', 'text.txt')
 
 try:
     file_obj = open(file_path)
