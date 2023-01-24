552fef8e882f69c150aaa7e1ddedfff3c3d86f4c
Anton Petrov <a.petrov.83@gmail.com>
2023-01-24 19:09:54 +0200

    Updated solution for exam "01. Multiplication Function" from "Functions Advanced - Lab"

--- 09_Functions_Advanced_Lab/01_multiplication_function.py
+++ 09_Functions_Advanced_Lab/01_multiplication_function.py
@@ -1,8 +1,13 @@
+# from functools import reduce
+
+
 def multiply(*args):
     result = 1
     for num in args:
         result *= num
     return result
+    # The code in function can be replaced with reduce!!! To use reduce should import reduce from functools
+    # return reduce(lambda a, b: a * b, args)
 
 
 print(multiply(1, 4, 5))
