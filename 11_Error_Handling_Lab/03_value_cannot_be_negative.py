1a08d895a9a94f6a877b23b085ca406ec78505dc
Anton Petrov <a.petrov.83@gmail.com>
2023-01-28 09:25:51 +0200

    Updated solution for exam

--- 11_Error_Handling_Lab/03_value_cannot_be_negative.py
+++ 11_Error_Handling_Lab/03_value_cannot_be_negative.py
@@ -5,7 +5,9 @@ class ValueCannotBeNegative(Exception):
     pass
 
 
-for i in range(5):
+numbers = [int(input()) for _ in range(5)]
+
+for num in numbers:
     number = int(input())
     if number < 0:
         raise ValueCannotBeNegative('The integer you provided is not a positive number')
