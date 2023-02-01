ebbb0e29e0736580861a9206b2adeea79edfd016
Anton Petrov <a.petrov.83@gmail.com>
2023-02-01 20:02:33 +0200

    Added solution for exam "01. Even Lines" from "File Handling - Exercise"

---
+++ 14_File_Handling_Exercise/01_even_lines.py
@@ -0,0 +1,9 @@
+symbols_to_replace = ["-", ",", ".", "!", "?"]
+file_path = 'text.txt'
+
+with open(file_path, 'r') as file:
+    for idx, line in enumerate(file):
+        if idx % 2 == 0:
+            for symbol in symbols_to_replace:
+                line = line.replace(symbol, '@')
+            print(' '.join(reversed(line.strip().split())))
