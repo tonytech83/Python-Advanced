6d5ffed8a4e3c55b7e3c99c52fba52c05491ee2c
Anton Petrov <a.petrov.83@gmail.com>
2023-02-02 15:30:40 +0200

    Added solution for "04. Directory Traversal" with build-in method os.walk

---
+++ 14_File_Handling_Exercise/04_Directory_Traversal/04_directory_traversal_build_in.py
@@ -0,0 +1,18 @@
+from os import walk
+
+start_path = input()
+files_by_ext = {}
+
+# uses build-in walk instead of recursion
+for _, _, files in walk(start_path):
+    for file in files:
+        extension = file.split('.')[-1]
+        if extension not in files_by_ext:
+            files_by_ext[extension] = []
+        files_by_ext[extension].append(file)
+
+with open('report.txt', 'w') as f:
+    for ext, files in sorted(files_by_ext.items()):
+        f.write(f'.{ext}\n')
+        for file in sorted(files):
+            f.write(f'- - - {file}\n')
