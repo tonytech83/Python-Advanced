edec28b345b7635b8f5059c60913adbe5e74b865
Anton Petrov <a.petrov.83@gmail.com>
2023-02-03 08:41:24 +0200

    Updated solution for exam "01. Even Lines" form "File Handling - Exercise"

--- 14_File_Handling_Exercise/01_even_lines.py
+++ 14_File_Handling_Exercise/01_even_lines.py
@@ -1,5 +1,5 @@
 symbols_to_replace = ["-", ",", ".", "!", "?"]
-file_path = '02_Line_Numbers/text.txt'
+file_path = 'files/text.txt'
 
 with open(file_path, 'r') as file:
     for idx, line in enumerate(file):
