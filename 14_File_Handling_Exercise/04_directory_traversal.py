83048cdcf40b9e92cc34c81991314a042d9c1f72
Anton Petrov <a.petrov.83@gmail.com>
2023-02-03 11:16:11 +0200

    Updated solution for exam "04. Directory Traversal" form "File Handling - Exercise"

--- 14_File_Handling_Exercise/04_directory_traversal.py
+++ 14_File_Handling_Exercise/04_directory_traversal.py
@@ -1,28 +1,36 @@
 import os
 
 
-def read_directory(current_directory: str):
+def extract_extensions(current_directory: str, first_level=False):
     """
-    This func traverses the first level of the directory only and adds information about each found file in dict.
+    This func traverses root directory and all first level directories.
+    Writes information about each found file to found_extensions dict.
     """
-    files = [item for item in os.listdir(current_directory) if os.path.isfile(item)]
+    global found_extensions
 
-    current_extensions = {}
+    for element in os.listdir(current_directory):
+        path = os.path.join(current_directory, element)
 
-    for file in files:
-        extension = file.split('.')[-1]
-        if extension not in current_extensions:
-            current_extensions[extension] = []
-        current_extensions[extension].append(file)
+        if os.path.isfile(path):
+            extension = element.split('.')[-1]
 
-    return current_extensions
+            if extension not in found_extensions:
+                found_extensions[extension] = []
+
+            found_extensions[extension].append(element)
+
+        elif os.path.isdir(element):
+            extract_extensions(path, first_level=True)
+
+        elif first_level:
+            break
 
 
 def create_report(extensions: dict):
     """
     This func writes information about each file form extensions dict in report.txt
     """
-    with open('report.txt', 'w') as f:
+    with open('files/report.txt', 'w') as f:
         for extension, files in sorted(extensions.items()):
             f.write(f'.{extension}\n')
             for file in sorted(files):
@@ -31,5 +39,6 @@ def create_report(extensions: dict):
 
 directory = input()
 
-found_extensions = read_directory(directory)
+found_extensions = {}
+extract_extensions(directory)
 create_report(found_extensions)
